981377b6559b88ab9047ee87ad05d20cc5a26b9e
Rajiv Lochan Sharma <rjvranjan80@gmail.com>
2025-11-24 14:49:22 +0530

    Fixed build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
+  // No experimental options needed for Next.js 14
 }
 
 module.exports = nextConfig
